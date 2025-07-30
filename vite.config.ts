6f16cdec5bf5396eade0436c1bd37aab888ffc6c
Innavoj <145022054+Innavoj@users.noreply.github.com>
2025-07-30 06:31:34 -0700

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+//import tailwindcss from '@tailwindcss';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+export default defineConfig(({ mode  }) => {
+     const env = loadEnv(mode, '.', '');
     return {
       plugins: [
-        tailwindcss(),
+        react(),
       ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -19,3 +20,4 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
